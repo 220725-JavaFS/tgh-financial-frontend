4f08a951f2cad49ece09a31fdd4d8dce7c38a58c
kirwi <kyleairwin@gmail.com>
2022-09-15 06:20:20 -0700

    edited environment.prod.ts file for production deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  // url of back end
+  url: 'http://localhost:8080/',
+  headers: {
+    'Content-Type': 'application/json',
+    // url of app
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Current-User': ''
+  },
+  withCredentials: true
 };
