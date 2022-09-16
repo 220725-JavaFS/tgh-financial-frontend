a6d244bd7885360e26dd928197d5845eb7148460
Dustin <110042715+DustinJae@users.noreply.github.com>
2022-09-16 16:49:47 -0500

    Update account.spec.ts

--- src/app/models/account.spec.ts
+++ src/app/models/account.spec.ts
@@ -2,6 +2,6 @@ import { Account } from './account';
 
 describe('Account', () => {
   it('should create an instance', () => {
-    expect(new Account(1, 'dustin', 1000, 'hello', 09-16-2022)).toBeTruthy();
+   expect(new Account()).toBeTruthy();
   });
 });
