27335ad7d12104bd9a3b0c715d4bed1b5eaf29ce
Max Morales <maxyMorales52@gmail.com>
2022-09-19 17:08:24 -0400

    Managed to git rid of duplicates in the testing to resolve the merge

--- src/app/models/tokenUser.spec.ts
+++ src/app/models/tokenUser.spec.ts
@@ -3,10 +3,6 @@ import { User } from './user';
 
 describe('TokenUser', () => {
   it('should create an instance', () => {
-<<<<<<< HEAD
-    expect(new TokenUser(0, '', new Date(), new User(0, '', ''))).toBeTruthy();
-=======
     expect(new TokenUser(0, '', new Date, new User(0, '', ''))).toBeTruthy();
->>>>>>> 50573b2b86370c34d5778c20eceb1b8e0f77ef58
   });
 });
