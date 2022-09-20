3261ccfef4e4024884a5dc183f2afdc1f095c9dc
smileykira237 <73151121+smileykira237@users.noreply.github.com>
2022-09-20 15:46:19 -0400

    Password reset (#40)
    * created the password-reset component
    
    * added reset-password component to app.module
    
    * inserted the password-reset component to the routes
    
    * started on the auth services implementation
    
    * Lockfileversion went back to 2. Was 1 for some reason.
    
    * started modifying password-reset.component.ts and auth.service.ts
    
    * Added The Password reset send email
    
    * Password-reset for sending the email for whom is in the database for them to reset with a token
    
    * Completed my password-reset component and services
    
    * testing example in authService
    
    * finished testing the services class
    
    * got rid of errors such as uncommented lines
    
    * added timeout to save password
    
    * Managed to git rid of duplicates in the testing to resolve the merge
    
    * Fixed merge conflics and completed spec testing using Karma and jasmin
    
    * minor changes
    
    * completed authService testing
    
    * Fixed a logic error and added component testing
    
    * got rid of the trim() method on password
    
    * fixed error message text color
    
    * added href for login redirect
    
    * Delete Password Reset Notes.rtf
    
    Co-authored-by: delay970 <60533032+delay970@users.noreply.github.com>
    Co-authored-by: Max Morales <maxyMorales52@gmail.com>
    Co-authored-by: Ryan Brooks <RJBComputerScientist@gmail.com>
    Co-authored-by: Kira Smiley <kira640@revature.net>
    Co-authored-by: delay970 <foxmatt26@gmail.com>
    Co-authored-by: Matthew Nelson <matthewnelson253@gmail.com>

---
+++ src/app/models/tokenUser.spec.ts
@@ -0,0 +1,8 @@
+import { TokenUser } from './tokenUser';
+import { User } from './user';
+
+describe('TokenUser', () => {
+  it('should create an instance', () => {
+    expect(new TokenUser(0, '', new Date(), new User(0, '', ''))).toBeTruthy();
+  });
+});
